cc7098b4d267d4345f2dbc043806e95b74d056cc
DiegoLG15 <a20200839@pucp.edu.pe>
2023-03-28 20:59:33 -0500

    Lab1 Grupo2

---
+++ Grupo2_LAB1_GTICS/src/main/java/com/example/grupo2_lab1_gtics/Grupo2Lab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo2_lab1_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo2Lab1GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo2Lab1GticsApplication.class, args);
+    }
+
+}
---
+++ Grupo2_LAB1_GTICS/src/test/java/com/example/grupo2_lab1_gtics/Grupo2Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo2_lab1_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2Lab1GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
